c7620fddb38f8f8cb0f5e38aad75dea61f716837
tpratyushareddy <50409898+tpratyushareddy@users.noreply.github.com>
2024-01-17 17:38:14 -0500

    Reading config files

--- cpu_usage_display.py
+++ cpu_usage_display.py
@@ -1,10 +1,21 @@
 import time
 import psutil 
+import yaml
 
-print(psutil.cpu_percent())
-print(psutil.virtual_memory().percent)
+#print(psutil.cpu_percent())
+#print(psutil.virtual_memory().percent)
 
-#print("what is max threshold ")
+#with open('config.yaml', 'r') as file:
+    #thresholds = yaml.safe_load(file)
+
+#print(thresholds[prime_numbers]['0'])
+#print(thresholds['mem_threshold'])
+
+with open('config.yml', 'r') as file:
+    prime_service = yaml.safe_load(file)
+
+print(prime_service['prime_numbers'][0])
+print(prime_service['rest']['url'])
 
 def display_usage(cpu_usage, mem_usage, bars=50):
     cpu_percent = (cpu_usage/100.0)
