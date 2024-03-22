d3842c8db2c4c617b8bfc3ae5236f31e622862e3
tpratyushareddy <50409898+tpratyushareddy@users.noreply.github.com>
2024-03-22 18:02:10 -0400

    chnages

--- Docker_cleanup.sh
+++ Docker_cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 action = $1
-if [ $action -eq images ]
+if [ "$action" == [images] ]
 then
     echo "Removing Images"
     docker rmi -f $(docker images -q)
-elif [ $action -eq container ]
+elif [ "$action" == [container] ]
 then
     echo "Removing Containers"
     docker rm -f $(docker ps -aq)
