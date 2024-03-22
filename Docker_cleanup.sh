ceaad518cd6a1fd36eef54d8bb01463889594ed0
tpratyushareddy <50409898+tpratyushareddy@users.noreply.github.com>
2024-03-22 17:11:49 -0400

    Docker Cleanup - To remove all images or containers

---
+++ Docker_cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+action = $1
+if [ action == images ]
+then
+    echo "Removing Images"
+    docker rmi -f $(docker images -q)
+elif [ action == container ]
+then
+    echo "Removing Containers"
+    docker rm -f $(docker ps -aq)
+else 
+    echo "Aurgumrent that has been passed is not valid, Please choose images or containers."
+fi
--- delete_all_images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "what is your name?"
-read name
-echo "How do you do, $name?"
-read remark
-echo "I am $remark too!"
-
-# still working on this 
