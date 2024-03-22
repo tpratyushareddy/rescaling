8d108764bcdb85d430c3d02f8ed33ff16a65758e
tpratyushareddy <50409898+tpratyushareddy@users.noreply.github.com>
2024-03-22 18:12:11 -0400

    updates

--- Docker_cleanup.sh
+++ Docker_cleanup.sh
@@ -10,5 +10,5 @@ then
     echo "Removing Containers"
     docker rm -f $(docker ps -aq)
 else 
-    echo "$action that has been passed is not valid, Please choose images or containers."
+    echo "$1 that has been passed is not valid, Please choose images or containers."
 fi
