fd4a1b10e29c311d85877931275cf656b9189c43
tpratyushareddy <50409898+tpratyushareddy@users.noreply.github.com>
2024-03-23 17:37:20 -0400

    modifying cleanup

--- Docker_cleanup.sh
+++ Docker_cleanup.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+set -e # Stop execution of the code if any of the steps fails
 
-
-if [ "$1" == [images] ]
+# Stores the passed argument
+action=$1
+# Comparing the argument with the string
+if [ action == "images" ]
 then
     echo "Removing Images"
+    #rmi is used to delete the images and docker images -q gives all the docker image ids. 
     docker rmi -f $(docker images -q)
-elif [ "$1" == [container] ]
+elif [ action == "containers" ]
 then
     echo "Removing Containers"
     docker rm -f $(docker ps -aq)
-else 
-    echo "$1 that has been passed is not valid, Please choose images or containers."
+else
+    echo "$action that has been passed is not valid, Please choose images or containers."
 fi
